62eadff10f2d048d1b9fa63a12a30fa7b140a011
Oli <Oli.boli@holi.trolly>
2019-10-02 16:08:52 +0100

    Commiting substring exercise

---
+++ src/JavaTest/StringIncrease.java
@@ -0,0 +1,18 @@
+package JavaTest;
+
+public class StringIncrease {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg = "the";
+		int i;
+		for(i=0;i<msg.length();i+=1) {
+			System.out.print(msg.substring(i,i+1));
+			System.out.print(msg.substring(i,i+1));
+			System.out.print(msg.substring(i,i+1));
+		}
+		
+			
+	}
+
+}
